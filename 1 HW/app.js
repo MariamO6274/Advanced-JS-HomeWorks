dc728f5ab56f0a12ef0295d9be24a3043a9c69c8
MariamO6274 <ostatishvili@gmail.com>
2023-04-21 17:29:31 -0600

    task assignment

---
+++ 1 HW/app.js
@@ -0,0 +1,13 @@
+// 1) Task
+
+// Write a function whitch takes a sentence as an input
+// example: my name is mariam
+// output: My Name Is Mariam
+
+
+// 2) Task
+
+// Question: Given an array with same duplicated elements
+// Out expected: Remove all the duplicate elements and keep unique entries
+// Example: input array = [2,4,5,6,2,9,4,2,7]
+// Expected output = [2,4,5,6,7,9]
