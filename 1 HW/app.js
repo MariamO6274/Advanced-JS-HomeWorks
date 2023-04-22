850c25c5fd8b2d93a7afcb9ef9d2bfb3ce728d69
MariamO6274 <ostatishvili@gmail.com>
2023-04-22 10:42:55 -0600

    task 1 and 2nd

--- 1 HW/app.js
+++ 1 HW/app.js
@@ -4,6 +4,15 @@
 // example: my name is mariam
 // output: My Name Is Mariam
 
+const myName = "my name is mariam";
+const arr = myName.split(" ");
+
+for (var i = 0; i < arr.length; i++) {
+  arr[i] = arr[i].charAt(0).toUpperCase() + arr[i].slice(1);
+}
+
+const newName = arr.join(" ");
+console.log(newName);
 
 // 2) Task
 
@@ -11,3 +20,11 @@
 // Out expected: Remove all the duplicate elements and keep unique entries
 // Example: input array = [2,4,5,6,2,9,4,2,7]
 // Expected output = [2,4,5,6,7,9]
+
+var array = [2, 4, 5, 6, 2, 9, 4, 2, 7];
+
+function removeDupli(array) {
+  return array.filter((item, index) => array.indexOf(item) === index);
+}
+
+console.log(removeDupli(array));
