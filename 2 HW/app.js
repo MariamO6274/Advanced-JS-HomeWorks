88ff96753872c729616bc973e039aaf5c54014a1
MariamO6274 <ostatishvili@gmail.com>
2023-04-23 15:37:07 -0600

    1 task/ 2 HW

--- 2 HW/app.js
+++ 2 HW/app.js
@@ -1,9 +1,32 @@
 // Date : 22 April 2023
 
 // 1. Write a JavaScript function that accepts a string as a parameter and finds the longest word within the string.
+
+function myString(str) {
+  var words = str.split(" ");
+  var longest = " ";
+  words.forEach((word) => {
+    if (word.length > longest.length) {
+      longest = word;
+    }
+  });
+  return longest;
+}
+console.log(myString("My name is Mariamii"));
+
+
+
 // 2. Write a JavaScript function that accepts a number as a parameter and checks whether it is prime or not.
+
 // 3. Write a JavaScript function that takes an array of numbers and finds the second lowest and second greatest numbers, respectively
+
 // 4. Write a JavaScript function to extract unique characters from a string.
+
 // 5. Write a JavaScript function to find the first not repeated character.
+
 // 6. Write a JavaScript program to pass a 'JavaScript function' as a parameter.
+
 // 7. Write a JavaScript function to get the function name.
+
+
+//prime divided only by 1 and itself 2, 3, 5, 
--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//April 20, 2023
 // 1) Task
 
 // Write a function whitch takes a sentence as an input
@@ -28,3 +29,41 @@ function removeDupli(array) {
 }
 
 console.log(removeDupli(array));
+
+/////////////////////////////////////////////////////////
+
+// 1. Write a JavaScript function that accepts a string as a parameter and finds the longest word within the string.
+
+
+function myString(str) {
+  var words = str.split(" ");
+  var longest = " ";
+  words.forEach((word) => {
+    if (word.length > longest.length) {
+      longest = word;
+    }
+  });
+  return longest;
+}
+console.log(myString("My name is Mariamooo"));
+
+
+// function findLongestWord(str) {
+//   var strSplit = str.split(" ");
+//   var longestWord = 0;
+//   for (var i = 0; i < strSplit.length; i++) {
+//     if (strSplit[i].length > longestWord) {
+//       longestWord = strSplit[i].length;
+//     }
+//   }
+//   return longestWord;
+// }
+// findLongestWord("The quick brown fox jumped over the lazy dog");
+
+// ////
+// var e_arr = [2, 4, 5, 6, 2, 9, 4, 2, 7];
+// var myUniqueElemSet = new Set();
+// e_arr.forEach((elem) => {
+//   myUniqueElemSet.add(elem);
+// });
+// console.log("myUnique Elements Set: ", myUniqueElemSet);
