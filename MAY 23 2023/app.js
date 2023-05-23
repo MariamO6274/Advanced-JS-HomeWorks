6c9047d15983bdc5f9d403f5abe12fd2bf9b2e81
MariamO6274 <ostatishvili@gmail.com>
2023-05-23 13:34:57 -0600

    image randomizer

---
+++ MAY 23 2023/app.js
@@ -0,0 +1,12 @@
+const imageArr = ["tester1.jpg", "tester3.jpg", "tester3.jpg"];
+
+function getRandomPhoto() {
+  //get a random index
+  var randomPhotoIndex = Math.floor(Math.random() * imageArr.length);
+
+  //get an image at the randomPhotoIndex
+  var selectedPhoto = imageArr[randomPhotoIndex];
+
+  //Display photo
+  document.getElementById("imageShower").src = `./images/${selectedPhoto}`;
+}
