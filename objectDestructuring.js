df8382d3dfef9add064f794600037e6d54b34128
MariamO6274 <ostatishvili@gmail.com>
2023-06-21 07:26:40 -0600

    array destructur and Brute Force method

--- objectDestructuring.js
+++ objectDestructuring.js
@@ -11,29 +11,27 @@ let [denberg, ny, phil] = arr1;
 // console.log(ny);
 // console.log(phil);
 
-
 // Object destructuring
 const obj = {
-    city1: "Denberg",
-    city2: "NY",
-    city3: "PHIL",
-}
+  city1: "Denberg",
+  city2: "NY",
+  city3: "PHIL",
+};
 
 // const obj1 = {
 //     data: {
 //         citiesInfo: {
-//             : 
+//             :
 //         }
 //     }
 // }
 
 // let denberg1 = obj.city1;
 
-
 // let ny1 = obj.city2;
 // let phil1 = obj.city3;
 // var {denberg1, ny1, phil1} = obj;
-var {city1, city2, city3 } = obj;
+var { city1, city2, city3 } = obj;
 
 console.log(city1);
 //console.log(ny1);
@@ -44,7 +42,7 @@ console.log(city1);
 // Array destrcuturing
 const numbers = [1, 2, 3, 4, 5, 6, 7];
 
-const [a, b,, ...rest] = numbers;
+const [a, b, , ...rest] = numbers;
 // const [a, b,, ...rest] = numbers; //[ 4, 5, 6, 7 ]
 // const [a, b, c, , d] = numbers; //skip
 
@@ -52,23 +50,54 @@ console.log(a); //1
 console.log(b); //2
 console.log(rest); //[ 3, 4, 5, 6, 7 ]
 
-
 // Object destructuring
 const user = {
-    firstName: "Mari",
-    lasName: "Ost",
-    age: 34,
-    city: undefined, // if Denver 
-}
+  firstName: "Mari",
+  lasName: "Ost",
+  age: 34,
+  city: undefined, // if Denver
+};
 
-const { firstName, age , city = "no city"} = user; //city will be Denver
+const { firstName, age, city = "no city" } = user; //city will be Denver
 console.log(firstName, age);
 
-console.log(city)
+console.log(city);
+
+//////////////////////////////////////////////////////////////
+// DESTRUCTURE
 
 
+const tree = {
+  value: 1,
+  children: [
+    { value: 2, children: [{ value: 3 }] },
+    { value: 4, children: [{ value: 5 }] },
+  ],
+};
+
+// output must be 
+// 1, 2, 3, 4, 5
+
+const {
+    value, 
+    children: [
+        { value: v2, children: [{value: v3}]},
+    {value: v4, children: [{value: v5}]}
+
+]
+} = tree
+console.log(value, v2, v3, v4, v5);
+
+// Let's do 
+// Brute Force method
+
+//somehow we need to print value
+
+let vv1 = tree.value;       // vv1 value as 1
+let vv2 = tree.children[0].value;       // first array object of children, this will give 2
+let vv3 = tree.children[0].children[0].value;       // we reched children in children's value which is 3
+let vv4 = tree.children[1].value;       // second children, children is an array with 2 objects, 
+let vv5 = tree.children[1].children[0].value;       //[0], [1] indexses
 
 
-
-////////////////////
-
+console.log("Brute Force method.." + vv1, vv2, vv3, vv4, vv5)
