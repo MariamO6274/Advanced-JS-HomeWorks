7ed294f7caf6a2af5e42786cb96a7b627f238877
MariamO6274 <ostatishvili@gmail.com>
2024-02-10 09:37:06 -0700

    adding more learn JS ex

--- 1learnJS.js
+++ 1learnJS.js
@@ -39,9 +39,50 @@ console.log(getFirstApp(["Chrome", "Clock", "Twitter"])); // "Chrome"
 console.log(getFirstApp(["Clock", "Contacts"])); // "Clock"
 
 // 4. Complete the function getLastApp such that it returns the last element from the apps array it receives as a parameter.
+function getLastApp(apps) {
+  return apps[apps.length - 1];
+}
+console.log(getLastApp(["Chrome", "Clock", "Twitter"])); // "Twitter"
+console.log(getLastApp(["Safari", "Contacts"])); // "Contacts"
 
+// ARRAYS
+// 5. Complete the function sumGrades such that it returns the sum of all the grades it receives as a parameter. We haven't seen reduce yet.
+function sumGrades(grades) {
+  let sum = 0;
+  grades.forEach(function (grade) {
+    sum += grade;
+  });
+  return sum;
+}
+console.log(sumGrades([15, 5, 10])); // 30
+console.log(sumGrades([12, 10, 13, 19])); // 54
 
+// 6. Complete the function sumPositiveNumbers such that it returns the sum of all positive numbers from the numbers parameter it receives.
+function sumPositiveNumbers(numbers) {
+  let sum = 0;
+  numbers.forEach(function (number) {
+    if (number >= 0) sum += number;
+  });
 
+  return sum;
+}
+console.log(sumPositiveNumbers([15, -5, 10])); // 25
+console.log(sumPositiveNumbers([-3, 4, -2, 1])); // 5
+
+// 7. Complete the function sumOddNumbers such that it returns the sum of all the odd numbers from the numbers parameter it receives.
+//The function should also work for negative numbers.
+function sumOddNumbers(numbers) {
+  let sum = 0;
+  numbers.forEach(function (number) {
+    if (number % 2 !== 0) {
+      sum += number;
+    }
+  });
+  return sum;
+}
+console.log(sumOddNumbers([15, 5, 10])); // 20
+console.log(sumOddNumbers([2, 3, 4, 5, 6])); // 8
+console.log(sumOddNumbers([-2, -3, 4, 5, 6])); // 2
 
 //================================================================
 // This is how we call it in terminal
